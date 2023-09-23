a770c86130311d5cf6ae31281615b840953ab9a5
Akash Mondal <akash2003mondal@gmail.com>
2023-09-23 20:32:48 +0530

    chat header

--- app/(main)/(route)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(route)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,3 +1,8 @@
+import ChatHeader from '@/components/chat/chat-header';
+import { currentProfile } from '@/lib/current-profile';
+import db from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import { FC } from 'react';
 
 interface ChannelIdProps {
@@ -6,7 +11,7 @@ interface ChannelIdProps {
     channelId: string;
   }
 }
-const ChannelId: FC<ChannelIdProps> = ({
+const ChannelId: FC<ChannelIdProps> = async ({
   params: {
     serverId,
     channelId
@@ -14,12 +19,33 @@ const ChannelId: FC<ChannelIdProps> = ({
 
 }) => {
 
+  const profile = await currentProfile()
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId
+    }
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId,
+      profileId: profile.id
+    }
+  })
+
+  if (!member || !channel) {
+    return redirect(`/servers/${serverId}`)
+  }
 
   return (
-    <div>
-      <h1>ChannelId</h1>
-      <p>serverId: {serverId}</p>
-      <p>channelId: {channelId}</p>
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader serverId={serverId} name={channel.name} type={'channel'}
+      //  imageUrl={channel.}
+        />
     </div>
   );
 };
